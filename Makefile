ca212421ea2f7b8f50ecccbacd5050e37fc2d055
Brett Letner <brett.letner@garmin.com>
2015-09-16 17:22:49 -0500

    some tree display

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all :
 	stack build
-	/home/bletner/git/ed/.stack-work/install/x86_64-linux/lts-3.5/7.10.2/bin/ed-exe
+#	~/git/ed/.stack-work/install/x86_64-linux/lts-3.5/7.10.2/bin/ed-exe
+	~/git/ed/.stack-work/install/i386-windows/lts-3.5/7.10.2/bin/ed-exe.exe
--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Control.Monad
 
 import qualified Graphics.UI.Threepenny as UI
-import Graphics.UI.Threepenny.Core hiding (get)
+import Graphics.UI.Threepenny.Core hiding (get, text)
 import Control.Concurrent.STM.TVar
 import Control.Monad.STM
 import Data.Char
 import Control.Monad.State
 import Data.List
+import Text.PrettyPrint hiding (style)
 
 {-----------------------------------------------------------------------------
     Main
@@ -17,16 +20,20 @@ main :: IO ()
 main = do
   startGUI defaultConfig setup
 
-canvasSize = 400
+canvasSize = 800
+
+lineHeight = 30
 
 setup window = do
   return window # set title "Canvas - Examples"
   bdy <- getBody window
-  tv <- liftIO $ atomically $ newTVar $ St Insert NoShift $ Z (reverse "hello, ") "world"
+  tv <- liftIO $ atomically $ newTVar $ St Move NoShift $ toZ a
+  -- tv <- liftIO $ atomically $ newTVar $ St Move NoShift $ Z (reverse "hello, ") "world."
   
   canvas <- UI.canvas
     # set UI.height canvasSize
     # set UI.width  canvasSize
+    # set UI.textFont "20px sans-serif"
     # set style [("border", "solid black 1px"), ("background", "#eee")]
 
   getBody window #+
@@ -36,8 +43,10 @@ setup window = do
   let disp i = do
         v <- liftIO $ atomically $ readTVar tv
         canvas # UI.clearCanvas
-        canvas # UI.fillText (pp $ zp v) (10,60)
-        canvas # UI.fillText (unwords [show i, show (move v), show (shift v)]) (10,120)
+        let ss = lines $ show $ pp $ zp v
+        sequence_ [ canvas # UI.fillText s (10,y) | (s,y) <- zip ss [60, 60 + lineHeight .. ] ]
+        -- canvas # draw (zp v) (10, 60)
+        canvas # UI.fillText (unwords [show i, show (move v), show (shift v)]) (10,520)
         
   on UI.keydown bdy $ \i -> do
     liftIO $ atomically $ modifyTVar tv $ execState (action i)
@@ -53,8 +62,6 @@ safeTail x = case x of
   "" -> ""
   _:bs -> bs
 
-moveRight (Z a b) = Z (safeHead b ++ a) (safeTail b)
-
 data MoveMode = Move | Insert deriving (Show, Eq)
 data ShiftMode = NoShift | Shift | CapsLock deriving (Show, Eq)
 
@@ -69,12 +76,11 @@ type M a = State St a
 action :: Int -> M ()
 action i = modify $ actionf i'
   where
+    i' = case lookup i keycodeTbl of
+      Just a -> fromEnum a
+      Nothing | isUpper c -> fromEnum $ toLower c
+      _ -> i
     c = toEnum i
-    i'
-      | isUpper c = fromEnum $ toLower c
-      | otherwise = case lookup i keycodeTbl of
-          Nothing -> i
-          Just c -> fromEnum c
 
 keycodeTbl :: [(Int, Char)]
 keycodeTbl =
@@ -89,8 +95,17 @@ keycodeTbl =
   , (220,'\\')
   , (221,']')
   , (222,'\'')
+  , (226,'\\')
   ]
-  
+
+-- moveBegin (Z a b) = Z [] $ reverse a ++ b
+-- moveEnd (Z a b) = Z (reverse b ++ a) []
+-- moveLeft (Z a b) = Z (safeTail a) (safeHead a ++ b)
+-- moveRight (Z a b) = Z (safeHead b ++ a) (safeTail b)
+
+-- del (Z a "") = Z (safeTail a) ""
+-- del (Z a b) = Z a (tail b)
+
 actionf :: Int -> St -> St
 actionf i st
   | i == 13 = case mv of
@@ -100,26 +115,28 @@ actionf i st
       NoShift -> st{ shift = Shift }
       Shift -> st{ shift = CapsLock }
       CapsLock -> st{ shift = NoShift }
+  -- | i == 46 = modZ del
   | mv == Move = case c of
-      'a' -> st{ zp = Z [] $ reverse a ++ b }
-      'f' -> st{ zp = Z (safeTail a) (safeHead a ++ b) }
-      'j' -> st{ zp = moveRight $ zp st }
-      ';' -> st{ zp = Z (reverse b ++ a) [] }
+      -- 'a' -> modZ moveBegin
+      -- 'f' -> modZ moveRight
+      -- 'j' -> modZ moveLeft
+      -- ';' -> modZ moveEnd
       _ -> st
-  | sh == NoShift = insert c
-  | sh == CapsLock = insert $ upper c
-  | otherwise = (insert $ upper c){ shift = NoShift }
+  -- | sh == NoShift = insert c
+  -- | sh == CapsLock = insert $ upper c
+  -- | otherwise = (insert $ upper c){ shift = NoShift }
   where
-    insert c = st{ zp = moveRight $ Z a (c : b) }
+    -- insert c = st{ zp = moveRight $ Z a (c : b) }
     upper c
       | isLower c = toUpper c
       | otherwise = case lookup c upperTbl of
           Nothing -> c
           Just c' -> c'
     sh = shift st
-    c = toEnum i
-    z@(Z a b) = zp st
+    c :: Char = toEnum i
+    -- z@(Z a b) = zp st
     mv = move st
+    -- modZ f = st{ zp = f $ zp st }
 
 upperTbl = zip "`1234567890-=[]\\;',./" "~!@#$%^&*()_+{}|:\"<>?"
 
@@ -135,11 +152,81 @@ upperTbl = zip "`1234567890-=[]\\;',./" "~!@#$%^&*()_+{}|:\"<>?"
 --   where
 --     c = toEnum i
 
-class PP a where pp :: a -> String
+-- class Draw a where draw :: a -> UI.Point -> UI.Canvas -> UI ()
+-- instance Draw Char where draw c = UI.fillText [c]
 
-data Z a = Z [a] [a]
+class PP a where pp :: a -> Doc
+
+instance PP Char where pp c = char c
+
+data Tree a = T a ([Tree a], [Tree a])
+  deriving Show
+
+data Z a = Z
+  { down :: ([Tree a], [Tree a])
+  , up :: [Tree a]
+  , horiz :: ([Tree a], [Tree a])
+  , val :: a
+  } deriving Show
+
+ppTree x ys = x $+$ (nest 4 $ vcat ys)
+
+instance PP a => PP (Tree a) where
+  pp (T a (bs, cs)) = ppTree (pp a) $ map pp bs ++ map pp cs
 
 instance PP a => PP (Z a) where
-  pp (Z a b) = concatMap pp (reverse a) ++ "_" ++ concatMap pp b
+  pp x = ppTree (hcat [ text ">", pp $ val x]) $ map pp $ lChilds x ++ rChilds x
+   
+-- instance Draw a => Draw (Tree a) where
+-- instance Draw a => Draw (Z a) where
 
-instance PP Char where pp c = [c]
+-- data Z a = Z [a] [a]
+-- instance PP a => PP (Z a) where
+--   pp (Z a b) = concatMap pp (reverse a) ++ "_" ++ concatMap pp b
+
+a = mkT 'a' [b,c]
+b = mkT 'b' []
+c = mkT 'c' [d,e]
+d = mkT 'd' []
+e = mkT 'e' []
+
+mkT a bs = T a ([], bs)
+
+toZ (T a b) = Z
+  { down = b
+  , up = []
+  , horiz = ([], [])
+  , val = a
+  }
+
+moveDown z = case down z of
+  ([], []) -> z
+  (e, T a b : ds) -> Z
+    { horiz = (e, ds)
+    , val = a
+    , up = T (val z) (horiz z) : up z
+    , down = b
+    }
+  _ -> moveLeft $ moveDown z
+
+left = fst . horiz
+right = snd . horiz
+lChilds = fst . down
+rChilds = snd . down
+
+moveUp z = case up z of
+  [] -> z
+  T a b : cs -> Z
+    { horiz = b
+    , val = a
+    , up = up z
+    , down = (left z, T (val z) (down z) : right z)
+    }
+
+moveLeft z = case horiz z of
+  (a : bs, c) -> z{ horiz = (bs, a : c) }
+  _ -> z
+
+moveRight z = case horiz z of
+  (bs, c : ds) -> z{ horiz = (c : bs, ds) }
+  _ -> z
