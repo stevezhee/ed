0e5740e201ab9fe9d5bfc329469c2f263ef03c73
Brett Letner <brett.letner@garmin.com>
2015-09-17 22:30:04 +0100

    stuff

--- app/Main.hs
+++ app/Main.hs
@@ -28,7 +28,7 @@ lineHeight = 30
 setup window = do
   return window # set title "Canvas - Examples"
   bdy <- getBody window
-  tv <- liftIO $ atomically $ newTVar $ St Move NoShift $ toZ a
+  tv <- liftIO $ atomically $ newTVar $ St minBound minBound $ toZTree a
   -- tv <- liftIO $ atomically $ newTVar $ St Move NoShift $ Z (reverse "hello, ") "world."
   
   canvas <- UI.canvas
@@ -60,17 +60,20 @@ safeHead x = case x of
 
 safeTail x = case x of
   "" -> ""
-  _:bs -> bs
+  _ : bs -> bs
 
-data MoveMode = Move | Insert deriving (Show, Eq)
-data ShiftMode = NoShift | Shift | CapsLock deriving (Show, Eq)
+data MoveMode = Move | Insert deriving (Show, Eq, Enum, Bounded)
+data ShiftMode = NoShift | Shift | CapsLock deriving (Show, Eq, Enum, Bounded)
 
 data St = St
   { move :: MoveMode
   , shift :: ShiftMode
-  , zp :: Z String
+  , zp :: ZTree (ZList Char)
   }
 
+instance PP a => PP (ZList a) where
+  pp (L bs cs) = hcat $ fmap pp (reverse bs) ++ [char '~'] ++ fmap pp cs
+    
 type M a = State St a
 
 action :: Int -> M ()
@@ -106,27 +109,30 @@ keycodeTbl =
 -- del (Z a "") = Z (safeTail a) ""
 -- del (Z a b) = Z a (tail b)
 
+incWrap :: (Enum a, Bounded a, Eq a) => a -> a
+incWrap x = if x == maxBound then minBound else succ x
+
 actionf :: Int -> St -> St
 actionf i st
-  | i == 13 = case mv of
-    Move -> st{ move = Insert }
-    Insert -> st{ move = Move }
-  | i == 16 = case sh of
-      NoShift -> st{ shift = Shift }
-      Shift -> st{ shift = CapsLock }
-      CapsLock -> st{ shift = NoShift }
+  | c == ' ' = st{ move = incWrap $ move st }
+  | i == 16 = st{ shift = incWrap $ shift st }
   -- | i == 46 = modZ del
   | mv == Move = case c of
       -- 'a' -> modZ moveBegin
-      'j' -> modZ moveRight
-      'f' -> modZ moveLeft
-      'k' -> modZ moveDown
-      'd' -> modZ moveUp
-      -- ';' -> modZ moveEnd
+      'k' -> modZ moveRight
+      'j' -> modL moveRight
+      'l' -> modZ moveDown
+      'd' -> modZ moveLeft
+      's' -> modZ moveUp
+      'f' -> modL moveLeft
       _ -> st
+  -- | mv == MoveVert = case c of
+  --     -- ';' -> modZ moveEnd
+  --     _ -> st
   -- | sh == NoShift = insert c
   -- | sh == CapsLock = insert $ upper c
   -- | otherwise = (insert $ upper c){ shift = NoShift }
+  | otherwise = st
   where
     -- insert c = st{ zp = moveRight $ Z a (c : b) }
     upper c
@@ -139,6 +145,7 @@ actionf i st
     -- z@(Z a b) = zp st
     mv = move st
     modZ f = st{ zp = f $ zp st }
+    modL f = modZ $ \z -> z{ val = f $ val z }
 
 upperTbl = zip "`1234567890-=[]\\;',./" "~!@#$%^&*()_+{}|:\"<>?"
 
@@ -150,7 +157,9 @@ instance PP a => PP [a] where pp = hcat . map pp
 data Tree a = T a ([Tree a], [Tree a])
   deriving (Show, Functor)
 
-data Z a = Z
+data ZList a = L [a] [a] deriving (Show, Functor)
+
+data ZTree a = Z
   { down :: ([Tree a], [Tree a])
   , up :: [Tree a]
   , horiz :: ([Tree a], [Tree a])
@@ -160,28 +169,33 @@ data Z a = Z
 instance PP Doc where pp = id
 
 instance PP a => PP (Tree a) where
-  pp (T a (bs, cs)) = pp a $+$ (nest 4 $ vcat $ map pp bs ++ map pp cs)
+  pp (T a (bs, cs)) = pp a $+$ (nest 4 $ vcat $ map pp (reverse bs) ++ map pp cs)
 
-instance PP a => PP (Z a) where
+instance PP a => PP (ZTree a) where
   pp x = pp $ T (val b) (down b)
    where
      a = fmap pp x
-     b = moveUp $ moveUp $ a{ val = hcat [ val a, text "<" ] }
+     b = moveUp $ moveUp $ moveUp $ a{ val = hcat [ val a, text "<" ] } -- bal: move up until the top
 
 -- data Z a = Z [a] [a]
 -- instance PP a => PP (Z a) where
 --   pp (Z a b) = concatMap pp (reverse a) ++ "_" ++ concatMap pp b
 
-a = mkT "hello" [b,c]
-b = mkT "world" []
-c = mkT "how" [d,e]
-d = mkT "are" []
-e = mkT "you?" []
+mkL x = L [] x
+
+mkLT a bs = mkT (mkL a) bs
+
+a = mkLT "hello" [b,c,f]
+b = mkLT "world" [mkLT "nice2" [], mkLT "meetu" []]
+c = mkLT "how" [d,e]
+d = mkLT "are" []
+e = mkLT "you?" []
+f = mkLT "I" [mkLT "am" [], mkLT "fine" []]
 
 mkT a bs = T a ([], bs)
 
-toZ :: Tree a -> Z a
-toZ (T a b) = Z
+toZTree :: Tree a -> ZTree a
+toZTree (T a b) = Z
   { down = b
   , up = []
   , horiz = ([], [])
@@ -191,6 +205,15 @@ toZ (T a b) = Z
 left = fst . horiz
 right = snd . horiz
 
+moveUp z = case up z of
+  [] -> z
+  T a b : cs -> Z
+    { val = a
+    , horiz = b
+    , up = cs
+    , down = (left z, unfocus z : right z)
+    }
+
 moveDown z = case down z of
   ([], []) -> z
   (e, T a b : ds) -> Z
@@ -199,29 +222,50 @@ moveDown z = case down z of
     , up = T (val z) (horiz z) : up z
     , down = b
     }
-  _ -> moveLeft $ moveDown z
+  (a:bs, []) -> moveDown $ z{ down = (bs, [a]) }
 
-moveUp z = case up z of
-  [] -> z
-  T a b : cs -> Z
-    { horiz = b
-    , val = a
-    , up = cs
-    , down = (left z, T (val z) (down z) : right z)
-    }
+class MoveHoriz a where
+  moveLeft :: a -> a
+  moveRight :: a -> a
 
-moveLeft z = case horiz z of
-  (T a b : cs, ds) -> z
-    { horiz = (cs, T (val z) (down z) : ds)
-    , val = a
-    , down = b
-    }
-  _ -> moveUp z
+unfocus z = T (val z) (down z)
 
-moveRight z = case horiz z of
-  (ds, T a b : cs) -> z
-    { horiz = (T (val z) (down z) : ds, cs)
-    , val = a
-    , down = b
-    }
-  _ -> moveDown z
+instance MoveHoriz (ZList a) where
+  moveLeft z = case z of
+    L [] cs -> L (reverse cs) []
+    L (a:bs) cs -> L bs (a:cs)
+  moveRight z = case z of
+    L cs [] -> L [] (reverse cs)
+    L cs (a:bs) -> L (a:cs) bs
+    
+instance MoveHoriz (ZTree a) where
+  moveLeft z = case horiz z of
+    ([], []) -> z
+    (T a b : cs, ds) -> z
+      { horiz = (cs, unfocus z : ds)
+      , val = a
+      , down = b
+      }
+    ([], ds) -> z
+      { horiz = (cs, [])
+      , val = a
+      , down = b
+      }
+      where
+      T a b : cs = reverse $ unfocus z : ds
+
+  moveRight z = case horiz z of
+    ([], []) -> z
+    (ds, T a b : cs) -> z
+      { horiz = (unfocus z : ds, cs)
+      , val = a
+      , down = b
+      }
+    (ds, []) -> z
+      { horiz = ([], cs)
+      , val = a
+      , down = b
+      }
+      where
+      T a b : cs = reverse $ unfocus z : ds
+    
